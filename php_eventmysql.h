b1e1c0ddc9e75585198d111a32e22ae5178ca87e
Timandes White <timands@gmail.com>
2016-09-26 22:01:12 +0800

    remove useless comments

--- php_eventmysql.h
+++ php_eventmysql.h
@@ -38,16 +38,6 @@ extern zend_module_entry eventmysql_module_entry;
 #include "TSRM.h"
 #endif
 
-/*
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:
-
-ZEND_BEGIN_MODULE_GLOBALS(eventmysql)
-	zend_long  global_value;
-	char *global_string;
-ZEND_END_MODULE_GLOBALS(eventmysql)
-*/
-
 /* Always refer to the globals in your function as EVENTMYSQL_G(variable).
    You are encouraged to rename these macros something shorter, see
    examples in any other php module directory.
